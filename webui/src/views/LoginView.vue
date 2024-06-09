9024902d7a66745db5e837c450bb7aa168133d82
lullu57 <kezitogaleacurmi@gmail.com>
2024-06-09 14:51:17 +0200

    attempt 2

--- webui/src/views/LoginView.vue
+++ webui/src/views/LoginView.vue
@@ -24,7 +24,7 @@ export default {
         const response = await api.post('/session', { name: this.username });
         localStorage.setItem("userId", response.data.token);
         axios.defaults.headers.common['Authorization'] = response.data;
-        window.location.href = '/stream'; // Redirects to /stream
+        this.$router.push('/stream'); // Redirects to /stream using Vue Router
       } catch (err) {
         this.error = 'Failed to login. Please try again.';
         console.error(err);
